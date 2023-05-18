bcccf16d1e3a8cc6b1164a86a267637cbf77ffd4
sewoolson <samwoolson@gmail.com>
2023-05-18 16:48:45 -0500

    corrected: 8.2 Express server added. 8.1 Node HTTP Server removed.

--- server/app.js
+++ server/app.js
@@ -1,13 +1,16 @@
 // 'Import' the http module
-const http = require("http");
 const express = require("express");
 const dotenv = require("dotenv");
-
 // Initialize the Express application
 const app = express();
 
 dotenv.config();
 
+app.get("/status", (request, response) => {
+  response.send(JSON.stringify({ message: "Service healthy" }));
+});
+
+app.listen(4040, () => console.log("Listening on port 4040"));
 
 //Logging Middleware
 const logging = (request, response, next) => {
@@ -33,22 +36,3 @@ app.use(cors);
 app.use(express.json());
 app.use(logging);
 
-// Initialize the http server
-const server = http.createServer((request, response) => {
-    // Handle the request from http://localhost:4040/status with HTTP GET method
-    if (request.url === "/status" && request.method === "GET") {
-      // Create the headers for response
-      response.writeHead(200, { "Content-Type": "application/json" });
-      // Create the response body
-      response.write(JSON.stringify({ message: "Service healthy" }));
-      // End and return the response
-      response.end();
-    }
-  })
-  // Tell the HTTP server to start listening
-  .listen(4040);
-
-
-
-// Let the humans know I am running and listening on 4040
-console.log("Listening on port 4040");
