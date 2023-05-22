cc0f6d428dbf0f47e7d3be938d0d2d794a8047c6
sewoolson <samwoolson@gmail.com>
2023-05-22 15:54:44 -0500

    revised May 22nd

--- server/app.js
+++ server/app.js
@@ -37,4 +37,4 @@ app.get("/status", (request, response) => {
   response.send(JSON.stringify({ message: "Service healthy" }));
 });
 
-app.listen(4040, () => console.log("Listening on port 4040"));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
