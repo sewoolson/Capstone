e9b75bbe983b38127629a054eea07dddaa6b6f51
sewoolson <samwoolson@gmail.com>
2023-06-02 17:03:44 -0500

    almost there

--- components/views/Community.js
+++ components/views/Community.js
@@ -5,19 +5,29 @@ import image3 from "../../assets/icons/userPosts/upvote.png";
 import image4 from "../../assets/icons/userPosts/downvote.png";
 import image5 from "../../assets/icons/userPosts/commentnum.png";
 import image6 from "../../assets/icons/userPosts/share.png";
+import image9 from "../../assets/illustrations/illustrationShe.png";
+import image8 from "../../assets/illustrations/illustrationThey.png";
+import image7 from "../../assets/illustrations/illustrationZir.png";
+import image10 from "../../assets/illustrations/illustrationHe.png";
 export default () => html`
-
+<div id="container3">
+<div class="illustrations">
+<img src="${image7}" alt="illustrationZir" id="image7"/>
+<img src="${image8}" alt="illustrationThey" id="image8"/>
+<img src="${image9}" alt="illustrationShe" id="image9"/>
+<img src="${image10}" alt="illustrationHe" id="image10"/>
+</div><!-- end illustration div -->
 
 <h2>Community Forum</h2>
 <p>Please be respectful to others. If you violate this you will be removed. If you see something please report it. Thank you and happy connecting!</p>
 <div id="create" class="post-container">
-<img src="${image1}" alt="createpost">
+<img src="${image1}" alt="createpost" id="createpost">
   <h2>Create Post</h2>
 </div>
 
 <div class="text-box">
   <div class="text-container">
-    <img src="${image2}" alt="userAvatar" class="userAvatar"><!-- check sizing in css -->
+    <img src="${image2}" alt="userAvatar" class="userAvatar">
     <p class="text-item" id="userName">Cale.125</p>
     <p class="text-item" id="date">10.26.2022</p>
   </div>
@@ -31,4 +41,5 @@ export default () => html`
   <img src="${image6}" alt="share">
   </div>
 </div>
+</div><!-- end text-container -->
 `;
